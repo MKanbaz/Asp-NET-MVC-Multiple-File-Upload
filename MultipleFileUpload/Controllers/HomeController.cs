f227a28b77b9648044897bd9eb542660d1fe0171
Mehmet KANBAZ <mkanbaz@outlook.com>
2019-07-25 10:29:55 +0300

    Add project files.

---
+++ MultipleFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultipleFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult MultipleUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MultipleUpload(HttpPostedFileBase[] files)
+        {
+            if (files != null)
+            {
+                foreach (HttpPostedFileBase file in files)
+                {
+                    file.SaveAs(Server.MapPath("~/Upload/" + file.FileName));
+                    ViewBag.State += "<br>" + file.FileName + " File Uploaded...";
+                }
+            }
+            return View();
+        }
+    }
+}
